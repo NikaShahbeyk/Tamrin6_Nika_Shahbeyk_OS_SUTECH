4cec518d0a85a378733716f5d53cef8c20168d0c
NikaShahbeyk <110238976+NikaShahbeyk@users.noreply.github.com>
2024-05-17 04:27:43 -0700

    Add files via upload

---
+++ autoresetevent/Program.cs
@@ -0,0 +1,25 @@
+﻿//Nika Shahbeyk_400213013_Auto Reset Event_Shiraz University Of Technology
+using System;
+using System.Threading;
+
+class Program
+ {
+        static AutoResetEvent a = new AutoResetEvent(true);
+        static void Main(String[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(withdrawMoney);
+                thread.Start(i + 1);
+            }
+            Console.ReadKey();
+        }
+        static void withdrawMoney(object obj)
+        {
+            a.WaitOne();
+            Console.WriteLine("Thread number" + obj.ToString(), Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            a.Set();
+        }
+ }
+
