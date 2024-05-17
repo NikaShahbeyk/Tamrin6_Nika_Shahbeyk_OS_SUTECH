2c9dc475a42ecc5b53b47d76165b2c114216cc73
NikaShahbeyk <110238976+NikaShahbeyk@users.noreply.github.com>
2024-05-17 04:33:06 -0700

    Add files via upload

---
+++ monitor/Program.cs
@@ -0,0 +1,24 @@
+﻿//Nika Shahbeyk_400213013_monitor_Shiraz University Of Technology
+using System;
+using System.Collections.Generic;
+class Program
+{
+        static object m = new object();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(withdrawMoney);
+                thread.Start(i+1);
+            }
+            Console.ReadKey();
+        }
+        static void withdrawMoney(object obj)
+        {
+            Monitor.Enter(m);
+            Console.WriteLine("Thread number " + obj.ToString());
+            Thread.Sleep(1000);
+            Monitor.Exit(m);
+          
+        }
+}
