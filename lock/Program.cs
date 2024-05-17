9325971d46e21f633350711b1bd0bfea178a950a
NikaShahbeyk <110238976+NikaShahbeyk@users.noreply.github.com>
2024-05-17 04:28:31 -0700

    Add files via upload

---
+++ lock/Program.cs
@@ -0,0 +1,28 @@
+﻿//Nika Shahbeyk_400213013_LOCK_Shiraz University Of technology
+using System;
+using System.Threading;
+
+class Program
+{
+        static object lock2 = new object();
+        static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(withdrawMoney);
+                thread.Start(i+1);
+            }
+            Console.ReadKey();
+        }
+
+        static void withdrawMoney(object obj)
+        {
+            lock (lock2)
+            {
+                Console.WriteLine("Thread number" + obj.ToString() , Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(2000);
+            }
+        }
+}
+
+
