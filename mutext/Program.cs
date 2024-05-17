9bba2cca44dea1f4af113cf02460c199d3b68be6
NikaShahbeyk <110238976+NikaShahbeyk@users.noreply.github.com>
2024-05-17 04:38:10 -0700

    Add files via upload

---
+++ mutext/Program.cs
@@ -0,0 +1,25 @@
+﻿//Nika Shahbeyk_Mutext_400213013_Shiraz University Of Technology
+using System;
+using System.Threading;
+
+class Program
+{
+        static Mutex mutext = new Mutex();
+        static void Main(String[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(withdrawMoney);
+                thread.Start(i+1);
+            }
+            Console.ReadKey();
+        }
+        static void withdrawMoney(object obj)
+        {
+            mutext.WaitOne();
+            Console.WriteLine("Thread number" + obj.ToString(), Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            mutext.ReleaseMutex();
+        }
+}
+
